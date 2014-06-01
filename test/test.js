f072520f940a3111afd0aa7ac331be5e7e127363
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-05-31 21:59:11 -0400

    tests: remove connect dependency

--- test/test.js
+++ test/test.js
@@ -1,267 +1,233 @@
-var assert = require('assert');
-var connect = require('connect');
+var http = require('http');
 var request = require('supertest');
 var should = require('should');
 
 var compress = require('..');
 
-var app = connect();
-app.use(compress({
-  threshold: 0
-}));
-
-app.use(connect.static(__dirname + '/..'));
-
-var app2 = connect();
-app2.use(compress({
-  threshold: '1kb'
-}));
-
-app2.use('/response/small', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('tiny');
-});
-
-app2.use('/response/large', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(new Buffer(2048));
-});
-
-app2.use('/response/writehead/object', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.writeHead(200, {'Content-Length': '2048', 'X-Custom': 'header'});
-  res.write(new Buffer(2048));
-  res.end();
-});
-
-app2.use('/response/writehead/array', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.writeHead(200, [['Content-Length', '2048'], ['X-Custom', 'array']]);
-  res.write(new Buffer(2048));
-  res.end();
-});
-
-app2.use('/stream/small/length', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', '1');
-  res.write('a');
-  res.end();
-});
-
-app2.use('/stream/large/length', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', '2048');
-  res.write(new Buffer(2048));
-  res.end();
-});
-
-app2.use('/stream/small', function(req, res, next){
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('a');
-  res.end();
-});
-
-app2.use('/image', function(req, res){
-  res.setHeader('Content-Type', 'image/png');
-  res.write(new Buffer(2048));
-  res.end();
-});
-
-app2.use('/vary', function(req, res){
-  res.setHeader('Vary', 'User-Agent');
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('a');
-  res.end();
-});
-
-app2.use('/vary-double', function(req, res){
-  res.setHeader('Vary', 'User-Agent, Accept-Encoding');
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('a');
-  res.end();
-});
-
 describe('compress()', function(){
   it('should gzip files', function(done){
-    request(app)
-    .get('/package.json')
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
     .set('Accept-Encoding', 'gzip')
-    .end(function(err, res){
-      res.body.should.not.equal('- groceries');
-      done();
-    });
+    .expect('Content-Encoding', 'gzip', done)
   })
 
-  it('should set Content-Encoding', function(done){
-    request(app)
-    .get('/package.json')
-    .set('Accept-Encoding', 'gzip')
-    .expect('Content-Encoding', 'gzip', done);
-  })
+  it('should skip HEAD', function(done){
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello, world')
+    })
 
-  it('should support HEAD', function(done){
-    request(app)
-    .head('/package.json')
+    request(server)
+    .head('/')
     .set('Accept-Encoding', 'gzip')
-    .expect('', done);
-  })
-
-  it('should support conditional GETs', function(done){
-    request(app)
-    .get('/package.json')
-    .set('Accept-Encoding', 'gzip')
-    .end(function(err, res){
-      var date = res.headers['last-modified'];
-      request(app)
-      .get('/package.json')
-      .set('Accept-Encoding', 'gzip')
-      .set('If-Modified-Since', date)
-      .expect(304, done);
-    });
+    .end(function (err, res) {
+      if (err) return done(err)
+      res.headers.should.not.have.property('content-encoding')
+      done()
+    })
   })
 
   it('should set Vary', function(done){
-    request(app)
-    .get('/package.json')
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
     .set('Accept-Encoding', 'gzip')
-    .expect('Vary', 'Accept-Encoding', done);
+    .expect('Content-Encoding', 'gzip')
+    .expect('Vary', 'Accept-Encoding', done)
   })
 
   it('should append to Vary', function(done){
-    request(app2)
-    .get('/vary')
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.setHeader('Vary', 'User-Agent')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
     .set('Accept-Encoding', 'gzip')
-    .expect('Vary', 'User-Agent, Accept-Encoding', done);
+    .expect('Content-Encoding', 'gzip')
+    .expect('Vary', 'User-Agent, Accept-Encoding', done)
   })
 
   it('should not double-append to Vary', function(done){
-    request(app2)
-    .get('/vary-double')
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.setHeader('Vary', 'Accept-Encoding, User-Agent')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
     .set('Accept-Encoding', 'gzip')
-    .expect('Vary', 'User-Agent, Accept-Encoding', done);
+    .expect('Content-Encoding', 'gzip')
+    .expect('Vary', 'Accept-Encoding, User-Agent', done)
   })
 
   it('should set Vary even if Accept-Encoding is not set', function(done){
-    request(app)
-    .get('/package.json')
-    .expect('Vary', 'Accept-Encoding', done);
+    var server = createServer({ threshold: 1000 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
+    .expect('Vary', 'Accept-Encoding')
+    .end(function (err, res) {
+      if (err) return done(err)
+      res.headers.should.not.have.property('content-encoding')
+      done()
+    })
   })
 
   it('should not set Vary if Content-Type does not pass filter', function(done){
-    request(app2)
-    .get('/image')
-    .end(function(err, res){
-      res.headers.should.not.have.property('vary');
-      done();
+    var server = createServer(null, function (req, res) {
+      res.setHeader('Content-Type', 'image/jpeg')
+      res.end()
+    })
+
+    request(server)
+    .get('/')
+    .end(function (err, res) {
+      if (err) return done(err)
+      res.headers.should.not.have.property('vary')
+      done()
     })
   })
 
   it('should transfer chunked', function(done){
-    request(app)
-    .get('/package.json')
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
     .set('Accept-Encoding', 'gzip')
-    .expect('Transfer-Encoding', 'chunked', done);
+    .expect('Transfer-Encoding', 'chunked', done)
   })
 
   it('should remove Content-Length for chunked', function(done){
-    request(app)
-    .get('/package.json')
-    .set('Accept-Encoding', 'gzip')
-    .end(function(err, res){
-      res.headers.should.not.have.property('content-length');
+    var server = createServer({ threshold: 0 }, function (req, res) {
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello, world')
+    })
+
+    request(server)
+    .get('/')
+    .expect('Content-Encoding', 'gzip')
+    .end(function (err, res) {
+      if (err) return done(err)
+      res.headers.should.not.have.property('content-length')
       done()
-    });
+    })
   })
-
-  describe('writehead', function(){
-    it('should support headers object', function(done){
-      request(app2)
-      .get('/response/writehead/object')
-      .set('Accept-Encoding', 'gzip')
-      .expect('X-Custom', 'header')
-      .expect('Content-Encoding', 'gzip', function(err, res){
-        if (err) return done(err);
-        assert.equal(res.headers['content-length'], undefined);
-        done();
-      });
-    })
-
-    it('support headers array', function(done){
-      request(app2)
-      .get('/response/writehead/array')
-      .set('Accept-Encoding', 'gzip')
-      .expect('X-Custom', 'array')
-      .expect('Content-Encoding', 'gzip', function(err, res){
-        if (err) return done(err);
-        assert.equal(res.headers['content-length'], undefined);
-        done();
-      });
-    })
-
-  });
 
   describe('threshold', function(){
     it('should not compress responses below the threshold size', function(done){
-      request(app2)
-      .get('/response/small')
+      var server = createServer({ threshold: '1kb' }, function (req, res) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Length', '12')
+        res.end('hello, world')
+      })
+
+      request(server)
+      .get('/')
       .set('Accept-Encoding', 'gzip')
       .end(function(err, res){
-        // I don't know how to do this with supertest
-        // '' or 'identity' should be valid values as well,
-        // but they are not set by compress.
-        assert.equal(res.headers['content-encoding'], undefined);
-
+        if (err) return done(err)
+        res.headers.should.not.have.property('content-encoding')
         done()
       })
     })
 
     it('should compress responses above the threshold size', function(done){
-      request(app2)
-      .get('/response/large')
+      var server = createServer({ threshold: '1kb' }, function (req, res) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Length', '2048')
+        res.end(new Buffer(2048))
+      })
+
+      request(server)
+      .get('/')
       .set('Accept-Encoding', 'gzip')
-      .expect('Content-Encoding', 'gzip', done);
+      .expect('Content-Encoding', 'gzip', done)
     })
 
     it('should compress when streaming without a content-length', function(done){
-      request(app2)
-      .get('/stream/small')
+      var server = createServer({ threshold: '1kb' }, function (req, res) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.write('hello, ')
+        setTimeout(function(){
+          res.end('world')
+        }, 10)
+      })
+
+      request(server)
+      .get('/')
       .set('Accept-Encoding', 'gzip')
-      .expect('Content-Encoding', 'gzip', done);
+      .expect('Content-Encoding', 'gzip', done)
     })
 
     it('should not compress when streaming and content-length is lower than threshold', function(done){
-      request(app2)
-      .get('/stream/small/length')
+      var server = createServer({ threshold: '1kb' }, function (req, res) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Length', '12')
+        res.write('hello, ')
+        setTimeout(function(){
+          res.end('world')
+        }, 10)
+      })
+
+      request(server)
+      .get('/')
       .set('Accept-Encoding', 'gzip')
       .end(function(err, res){
-        assert.equal(res.headers['content-encoding'], undefined);
-
+        if (err) return done(err)
+        res.headers.should.not.have.property('content-encoding')
         done()
       })
     })
 
     it('should compress when streaming and content-length is larger than threshold', function(done){
-      request(app2)
-      .get('/stream/large/length')
+      var server = createServer({ threshold: '1kb' }, function (req, res) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Length', '2048')
+        res.write(new Buffer(1024))
+        setTimeout(function(){
+          res.end(new Buffer(1024))
+        }, 10)
+      })
+
+      request(server)
+      .get('/')
       .set('Accept-Encoding', 'gzip')
-      .expect('Content-Encoding', 'gzip', done);
+      .expect('Content-Encoding', 'gzip', done)
     })
   })
 
   describe('res.flush()', function () {
     it('should always be present', function (done) {
-      var app = connect();
+      var server = createServer(null, function (req, res) {
+        res.statusCode = typeof res.flush === 'function'
+          ? 200
+          : 500
+        res.end()
+      })
 
-      app.use(compress());
-      app.use(function (req, res) {
-        res.flush.should.be.a.Function;
-        res.statusCode = 204;
-        res.end();
-      });
-
-      request(app)
+      request(server)
       .get('/')
-      .expect(204, done);
+      .expect(200, done)
     })
 
     // If anyone knows how to test if the flush works...
@@ -270,3 +236,18 @@ describe('compress()', function(){
     // })
   })
 })
+
+function createServer(opts, fn) {
+  var _compress = compress(opts)
+  return http.createServer(function (req, res) {
+    _compress(req, res, function (err) {
+      if (err) {
+        res.statusCode = err.status || 500
+        res.end(err.message)
+        return;
+      }
+
+      fn(req, res)
+    })
+  })
+}
